5e0d6dde1fa9b309d8a4faff8ab7a2bab9c7783a
Ramchandra Tharu <ramchandrachaudhary2003@gmail.com>
2025-11-24 23:53:19 +0530

    new features added

---
+++ app/contact/page.tsx
@@ -0,0 +1,8 @@
+export default function Contact() {
+  return (
+    <div className="p-10">
+      <h1 className="text-4xl font-bold mb-4">Contact Me</h1>
+      <p>Email: sirzanchaudhary@gmail.com</p>
+    </div>
+  );
+}
